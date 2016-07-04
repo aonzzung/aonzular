e17f8af0d2380ae2c1fd390970934c42f36c6fde
atthaponj <atthaponj@BIT-PC0008.bred-it.local>
2016-07-04 18:31:55 +0700

    first aonzular commit

---
+++ src/main/java/com/aonzular/AonzularApplication.java
@@ -0,0 +1,12 @@
+package com.aonzular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AonzularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AonzularApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aonzular/AonzularApplicationTests.java
@@ -0,0 +1,18 @@
+package com.aonzular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AonzularApplication.class)
+@WebAppConfiguration
+public class AonzularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
